f22041fdc5cf5efd179927fe86493a50a2101fd5
Tukkos <jpfcastro@id.uff.br>
2022-11-19 14:23:40 -0300

    refactor: get /payments route refactored;

--- src/controllers/payments-controller.ts
+++ src/controllers/payments-controller.ts
@@ -6,12 +6,24 @@ import httpStatus from "http-status";
 
 export async function getPayments(req: AuthenticatedRequest, res: Response) {
   const { ticketId } = req.query;
-  const { userId } = req;
   try {
     if (ticketId === undefined) {
       return res.sendStatus(httpStatus.BAD_REQUEST);
     }
 
+    await ticketVallidation(req, res);
+
+    const result = await paymentsService.getPaymentResponse(Number(ticketId));
+    return res.status(httpStatus.OK).send(result);   
+  } catch (error) {
+    return res.sendStatus(httpStatus.INTERNAL_SERVER_ERROR);
+  }
+}
+
+export async function ticketVallidation(req: AuthenticatedRequest, res: Response) {
+  const { ticketId } = req.query;
+  const { userId } = req;
+  try {
     const ticket = await ticketsService.getTicketResponseByTitcketId(Number(ticketId));
     if (!ticket) {
       return res.sendStatus(httpStatus.NOT_FOUND);
@@ -21,9 +33,6 @@ export async function getPayments(req: AuthenticatedRequest, res: Response) {
     if (!userTicket) {
       return res.sendStatus(httpStatus.UNAUTHORIZED);
     }
-
-    const result = await paymentsService.getPaymentResponse(Number(ticketId));
-    return res.status(httpStatus.OK).send(result);   
   } catch (error) {
     return res.sendStatus(httpStatus.INTERNAL_SERVER_ERROR);
   }
